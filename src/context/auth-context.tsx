9238aae0bdba1704e913bd1d362b1ab8f9734783
Jiani Liu <liujianiljn@gmail.com>
2022-12-03 18:11:17 -0800

    feat: able to keep login status and finish majority of the login system

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode, useState } from "react";
 import { User } from "screens/project-list/search-panel";
 import * as auth from "auth-provider";
+import { http } from "utils/http";
+import { useMount } from "utils";
 interface AuthForm {
   username: string;
   password: string;
@@ -16,13 +18,24 @@ const AuthContext = React.createContext<
 >(undefined);
 
 AuthContext.displayName = "AuthContext";
-
+const bootstrapUser = async () => {
+  let user = null;
+  const token = auth.getToken();
+  if (token) {
+    const data = await http("me", { token });
+    user = data.user;
+  }
+  return user;
+};
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   //point free
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
+  useMount(() => {
+    bootstrapUser().then(setUser);
+  });
   return (
     <AuthContext.Provider
       children={children}
