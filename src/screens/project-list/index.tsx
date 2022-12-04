c00f4a9dd4ebb3f5f0407ea7ae60c01f8ea27b09
Jiani Liu <liujianiljn@gmail.com>
2022-12-03 17:17:11 -0800

    feat: add HTTP client abstraction and carry token in request

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import * as qs from "qs";
 import { cleanObject, useDebounce, useMount } from "utils";
+import { useHttp } from "utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -14,21 +15,12 @@ export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const debouncedParam = useDebounce(param, 300);
   const [list, setList] = useState([]);
+  const client = useHttp();
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
   return (
     <div>
---
+++ src/utils/http.ts
@@ -0,0 +1,56 @@
+import qs from "qs";
+import * as auth from "auth-provider";
+import { useAuth } from "context/auth-context";
+const apiUrl = process.env.REACT_APP_API_URL;
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+//endpoint, like login/project
+//config add defult value: so config could be empty
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config = {}
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      //Bearer is the standard begining form of token
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+  //axios and fetch:
+  //axios will return all error status that is not 200
+  //fetch will not
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      //401 unauthorized
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "Please try to login again." });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+        //manutally report when there is error like internet error connection
+        //regular error like 404 will belong to response.ok
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
+//custom hook ( since we want to use hook )
+export const useHttp = () => {
+  const { user } = useAuth();
+  return (...[endpoint, config]: Parameters<typeof http>) =>
+    http(endpoint, { ...config, token: user?.token });
+};
