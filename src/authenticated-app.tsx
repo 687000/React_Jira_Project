9efeb9c31641dff14ab44ecde90185e0bb5b2f5c
Jiani Liu <liujianiljn@gmail.com>
2022-12-05 20:34:54 -0800

    style: use Row to improve the layout

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,26 +1,24 @@
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>Logo</h3>
-          <h3>Projects</h3>
-          <h3>Users</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>Projects</h2>
+          <h2>Users</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>Logout</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
@@ -28,38 +26,15 @@ export const AuthenticatedApp = () => {
 //grid-template-rows: layout for rows
 //grid-template-areas: describe the layout elements
 const Container = styled.div`
-  display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem auto;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
   height: 100vh;
-  grid-gap: 2rem;
 `;
 //grid-area: name the child element of grid
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-const HeaderLeft = styled.div`
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)`
   display: flex;
   align-items: center;
 `;
 const HeaderRight = styled.div``;
 const Main = styled.main`
   grid-area: main;
-`;
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-const Footer = styled.footer`
-  grid-area: footer;
 `;
