4fcc2ebb4029f9e2a72bed351b71a839dbfa14b8
Jiani Liu <liujianiljn@gmail.com>
2023-02-02 08:10:48 -0800

    style: add loading when opening the dashboard

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -6,23 +6,34 @@ import {
   useDashboardSearchParams,
   useProjectIdInUrl,
   useProjectInUrl,
+  useTasksSearchParams,
 } from "./util";
 import styled from "@emotion/styled";
 import { SearchPanel } from "./search-panel";
 import { ScreenContainer } from "components/lib";
+import { useTasks } from "utils/task";
+import { Spin } from "antd";
 export const DashboardScreen = () => {
   useDocumentTitle("Dashboard Lists");
   const { data: currentProject } = useProjectInUrl();
-  const { data: dashboards } = useDashboards(useDashboardSearchParams());
+  const { data: dashboards, isLoading: dashboardIsLoading } = useDashboards(
+    useDashboardSearchParams()
+  );
+  const { isLoading: taskIsLoading } = useTasks(useTasksSearchParams());
+  const isLoading = dashboardIsLoading || taskIsLoading;
   return (
     <ScreenContainer>
       <h1>{currentProject?.name} Dashboards</h1>
       <SearchPanel />
-      <ColumnsContainer>
-        {dashboards?.map((dashboard) => (
-          <DashboardColumn dashboard={dashboard} key={dashboard.id} />
-        ))}
-      </ColumnsContainer>
+      {isLoading ? (
+        <Spin size={"large"} />
+      ) : (
+        <ColumnsContainer>
+          {dashboards?.map((dashboard) => (
+            <DashboardColumn dashboard={dashboard} key={dashboard.id} />
+          ))}
+        </ColumnsContainer>
+      )}
     </ScreenContainer>
   );
 };
