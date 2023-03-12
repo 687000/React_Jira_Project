b4953c98c66c7202bc67563748b3a50f05e87816
jiani liu <liujianiljn@gmail.com>
2023-03-11 20:42:59 -0800

    feat: update jira-dev-tool and error translator

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -47,7 +47,11 @@ const isError = (value: any): value is Error => value?.message;
 
 export const ErrorBox = ({ error }: { error: unknown }) => {
   if (isError(error)) {
-    return <Typography.Text type={"danger"}>{error?.message}</Typography.Text>;
+    return (
+      <Typography.Text type={"danger"}>
+        {TranslateErrMsg(error?.message)}
+      </Typography.Text>
+    );
   }
   return null;
 };
